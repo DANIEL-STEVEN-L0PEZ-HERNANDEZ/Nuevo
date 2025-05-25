a254a211854385e63755a25762ffa41c1d46faa2
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-05-24 19:50:21 -0600

    Primer commit

---
+++ FrutasInversas.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FrutasInversas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] frutas = new String[10];
+
+        // Pedir al usuario que ingrese las 10 frutas
+        System.out.println("Ingrese 10 nombres de frutas:");
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.print("Fruta " + (i + 1) + ": ");
+            frutas[i] = scanner.nextLine();
+        }
+
+        // Mostrar las frutas en orden inverso
+        System.out.println("\nFrutas en orden inverso:");
+        for (int i = frutas.length - 1; i >= 0; i--) {
+            System.out.println(frutas[i]);
+        }
+        scanner.close();
+    }
+}
